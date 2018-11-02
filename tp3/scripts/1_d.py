e61a91b544763d185e31a08f2ea67fc440274402
andres1074 <42684653+andres1074@users.noreply.github.com>
2018-11-02 18:14:01 -0300

    Update 1_d.py

--- tp3/scripts/1_d.py
+++ tp3/scripts/1_d.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -15,12 +17,14 @@ def ldata(archive):
     
 generos = ldata("../dataset/dolphinsGender.txt")
 
-#particiones = ["../dataset/particion_louvain.txt", "../dataset/particion_edge_betweenness.txt", "../dataset/particion_fast_greedy_andres.txt", "../dataset/particion_infomap.txt"]
-particiones = ["../dataset/particion_louvain.txt"]
+particiones = ["../dataset/particion_louvain.txt", "../dataset/particion_edge_betweenness.txt", "../dataset/particion_fast_greedy_andres.txt", "../dataset/particion_infomap.txt"]
 
 
 label_particion = ["Louvain", "Edge Betweenness", "Fast Greedy", "Infomap"]
 
+# txt para guardar los p-valor
+results = open("../p_valor.txt", "w")
+    
 # itero sobre las distintas particiones
 for j in range(len(particiones)):
 
@@ -160,8 +164,6 @@ for j in range(len(particiones)):
 
     # grafico histogramas
     
-    
-    
     # machos
     for i in range(cant_grupos):
         nombre = "../plots/" + label_particion[j] + " - Histograma machos - Grupo " + str(i) + ".png"
@@ -178,31 +180,77 @@ for j in range(len(particiones)):
         plt.axvline(x = generos_en_grupos[i][1], color='red', label='Experimental')
         
         # señalo el maximo
-        plt.axvline(x = mean_dist, color='green')
+        #plt.axvline(x = mean_dist, color='orange', label='Valor medio')
+        
+        # ademas calculo el p-valor
+        # tengo que ver donde esta el maximo de la distribucion
+        # recorro hist_m[i] y separo aquellos que son mas extremos que el valor experimental
+        # me fijo si el valor experimental esta a la derecha o a la izq:
+        if generos_en_grupos[i][1] > mean_dist:
+            extremos = []
+            for h in hist_m[i]:
+                if h > generos_en_grupos[i][1]:
+                    extremos.append(h)
+            plt.hist(extremos, color='green', label='Extremos')
+        else:
+            extremos = []
+            for h in hist_m[i]:
+                if h < generos_en_grupos[i][1]:
+                    extremos.append(h)
+            plt.hist(extremos, color='green', label='Extremos')
+        
+        # para cada i tengo una lista de puntos extremos
+        # los cuento y divido por el numero total de cuentas
+        cuentas_totales = len(hist_m[i])
+        cuentas_extremas = len(extremos)
+        
+        p_valor = cuentas_extremas/cuentas_totales
+        
+        results.write("%s - Machos - Grupo %d - p-valor = %.3f\n" % (label_particion[j], i, p_valor))
         
         plt.xlabel("fraccion de machos")
-        plt.suptitle(titulo)
+        plt.suptitle(titulo + " - p-valor = %.3f" % (p_valor))
         plt.legend()
         plt.savefig(nombre)
         plt.close(fig)
         
-        # ademas calculo el p-valor
-        # tengo que ver donde esta el maximo de la distribucion
-        
-        
-    
     # hembras
     for i in range(cant_grupos):
         nombre = "../plots/" + label_particion[j] + " - Histograma hembras - Grupo " + str(i) + ".png"
         titulo = label_particion[j] + " - Grupo " + str(i) + "\nN = " + str(N)
+        
+        mean_dist = mean(hist_f[i])
         
         fig = plt.figure()
         #plt.hist(hist_f[i], bins = linspace(0, 1, 25), label='Modelo nulo')
         plt.hist(hist_f[i], label='Modelo nulo')
         # grafico el valor experimental
         plt.axvline(x = generos_en_grupos[i][2], color='red', label='Experimental')
+        
+        #plt.axvline(x = mean_dist, color='orange', label='Valor medio')
+        
+        if generos_en_grupos[i][2] > mean_dist:
+            extremos = []
+            for h in hist_f[i]:
+                if h > generos_en_grupos[i][2]:
+                    extremos.append(h)
+            plt.hist(extremos, color='green', label='Extremos')
+        else:
+            extremos = []
+            for h in hist_f[i]:
+                if h < generos_en_grupos[i][2]:
+                    extremos.append(h)
+            plt.hist(extremos, color='green', label='Extremos')
+        
+        cuentas_totales = len(hist_f[i])
+        cuentas_extremas = len(extremos)
+        
+        p_valor = cuentas_extremas/cuentas_totales
+        
+        results.write("%s - Hembras - Grupo %d - p-valor = %.3f\n" % (label_particion[j], i, p_valor))
+        
         plt.xlabel("fraccion de hembras")
-        plt.suptitle(titulo)
+        plt.suptitle(titulo + " - p-valor = %.3f" % (p_valor))
         plt.legend()
         plt.savefig(nombre)
         plt.close(fig)
@@ -212,14 +260,20 @@ for j in range(len(particiones)):
         nombre = "../plots/" + label_particion[j] + " - Histograma NA - Grupo " + str(i) + ".png"
         titulo = label_particion[j] + " - Grupo " + str(i) + "\nN = " + str(N)
         
+        mean_dist = mean(hist_na[i])
+        
         fig = plt.figure()
         #plt.hist(hist_na[i], bins = linspace(0, 1, 25), label='Modelo nulo')
         plt.hist(hist_na[i], label='Modelo nulo')
         # grafico el valor experimental
         plt.axvline(x = generos_en_grupos[i][3], color='red', label='Experimental')
+        
+        plt.axvline(x = mean_dist, color='orange')
+        
         plt.xlabel("fraccion de NA")
         plt.suptitle(titulo)
         plt.legend()
         plt.savefig(nombre)
         plt.close(fig)
 
+results.close()
