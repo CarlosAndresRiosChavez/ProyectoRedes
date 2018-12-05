36b7d259a542738f5fe1ba23d45caa5c72027f97
andres1074 <42684653+andres1074@users.noreply.github.com>
2018-12-05 12:51:45 -0300

    Update enriquecimiento_master.py

--- tp4/enriquecimiento_comunidades/enriquecimiento_master.py
+++ tp4/enriquecimiento_comunidades/enriquecimiento_master.py
@@ -4,11 +4,14 @@ from enriquecimiento_1 import etapa_1
 from enriquecimiento_2 import etapa_2
 from enriquecimiento_3 import etapa_3
 from enriquecimiento_4 import etapa_4
+from separar_resultados import sep
 
 input_files = glob.glob("./comunidades/*.txt")
 
+# por que './3/total_lines_GO_por_categoria.txt' es la misma aun cuando le paso distintas comunidades????
+
 # itero sobre las comunidades
-for f in input_files[:4]:
+for f in input_files:
     #print(f)
     
     # quiero extraer el numero de comunidad
@@ -24,7 +27,7 @@ for f in input_files[:4]:
     print("=============")
     
     print("Etapa 0")
-    etapa_0(f, numero_de_comunidad)
+    size_comunidad = etapa_0(f, numero_de_comunidad)
     
     print("Etapa 1")
     etapa_1(numero_de_comunidad)
@@ -36,6 +39,8 @@ for f in input_files[:4]:
     etapa_3(numero_de_comunidad)
     
     print("Etapa 4")
-    etapa_4(numero_de_comunidad)
+    etapa_4(numero_de_comunidad, size_comunidad)
     
     print()
+    
+sep()
